4b0b98b355812e5cec8794ecbc69de97ff3013e6
TopEngineer0926 <top_engineer_0926@outlook.com>
2022-05-12 11:00:10 -0400

    Add test code

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals'
 import App from './app'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+console.log('ssssssssssss')
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
